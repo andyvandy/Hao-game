486b63f582aa8a80b90a01065581ea1456e5b619
Andrew <vandenhoeven.andrew@gmail.com>
2016-11-13 13:23:07 -0500

    Added a basic window

--- game.cpp
+++ game.cpp
@@ -3,11 +3,34 @@ This is the main controller where we will parse user inputs in the gmaeloop
 
 */
 #include "character.h"
+
 #include  <SFML/Graphics.hpp>
+#include <string>
 
 
+void gameloop(){
+    int width= 200;
+    int height= 200;
+    sf::RenderWindow window(sf::VideoMode(width, height), "Game prototype!");
+    sf::CircleShape circle(50);
+    while (1)
+    {   
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(circle);
+        window.display();
+        if(!window.isOpen()){
+            break;
+        }
+    }
+}
 
 int main(){
-
+    gameloop();
     return 0;
 }
